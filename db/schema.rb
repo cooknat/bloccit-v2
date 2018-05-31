1e75c3d9e21128ba3c9429ee61aa8ba833b692f4
cooknat <natalie.t.cook@gmail.com>
2018-05-31 21:08:49 +0100

    answer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180531195455) do
+ActiveRecord::Schema.define(version: 20180531200822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -40,5 +49,6 @@ ActiveRecord::Schema.define(version: 20180531195455) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "comments", "posts"
 end
